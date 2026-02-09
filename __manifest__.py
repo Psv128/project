f80a47a5d977fdd41c776f98d341302a05c92b0f
Psv128 <psvsolis@gmail.com>
2026-02-09 13:32:06 +0100

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Academy Management",
+    "version": "1.0",
+    "category": "Education",
+    "summary": "Gestión de cursos y alumnos",
+    "description": "Módulo para gestionar cursos y alumnos en una academia.",
+    "author": "Tu Nombre",
+    "depends": ["base"],
+    "data": [
+        "views/course_views.xml",
+        "views/student_views.xml",
+    ],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
